a113091a94e01e42f9307be2c568edbcf506d2e7
abachant <gbachant@gmail.com>
2020-05-09 22:45:49 -0400

    Add leaflet <Map /> to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      test
+      <div className="container-fluid" id="map-container">
+        <Map className="map" center={[39.8283, -98.5795]} zoom={5}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+        </Map>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function NavBar() {
   return (
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="navbar-brand">Lane Claim</div>
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="navbar-brand">Lane Claim</div>
         <div>
-            <a href="#" id="aboutButton">about</a>
-            <button class="btn btn-success" type="button">Submit New Claim</button>
+            <button className="btn">about</button>
+            <button className="btn btn-success" type="button">Submit New Claim</button>
         </div>
     </nav>
   );
