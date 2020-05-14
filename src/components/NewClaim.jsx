520a9696689037ff92c61b714d92a66094e1aacf
abachant <gbachant@gmail.com>
2020-05-13 21:02:37 -0400

    Add basic modals for each step of submission process

--- src/components/NewClaim.jsx
+++ src/components/NewClaim.jsx
@@ -3,18 +3,34 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
 function NewClaim() {
-    const [show, setShow] = useState(false);
+    const [showUploadModal, setUploadModal] = useState(false);
+    const [showConfirmationModal, setConfirmationModal] = useState(false);
+    const [showSuccessModal, setSuccessModal] = useState(false);
+    
+    const hideUploadModal = () => setUploadModal(false);
+    const openUploadModal = () => setUploadModal(true);
+    const hideConfirmationModal = () => setConfirmationModal(false);
+    const openConfirmationModal = () => setConfirmationModal(true);
+    const hideSuccessModal = () => setSuccessModal(false);
+    const openSuccessModal = () => setSuccessModal(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    // const [activeModals, setActiveModals] = useState({"upload-modal": false, "confirmation-modal": false, "success-modal": false})
+
+    // const toggleModal = (x) => {setUploadModal(x)};
+    // const foo = (modal, value) => {
+    //     const s = {...activeModals};
+    //     s[modal] = value;
+    //     setActiveModals(s);
+    // }
+
 
     return (
         <>
-            <Button variant="success" onClick={handleShow}>
+            <Button variant="success" onClick={openUploadModal}>
                 Submit New Claim
             </Button>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={showUploadModal} onHide={hideUploadModal} id="upload-modal" animation={false}>
                 <Modal.Header closeButton>
                     <Modal.Title>Submit New Claim</Modal.Title>
                 </Modal.Header>
@@ -26,11 +42,104 @@ function NewClaim() {
                     <p>File does not contain GPS metadata</p>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button variant="secondary" onClick={hideUploadModal}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={() =>{hideUploadModal(); openConfirmationModal();}}>
                         Next
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showConfirmationModal} onHide={hideConfirmationModal} id="confirmation-modal" animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        License Plate:<br />
+                        <input type="text" id="licensePlate" /><br />
+                        <label htmlFor="state-selector">License State:</label>
+                        <select className="form-control" id="state-selector">
+                            <option value="N/A">N/A</option>
+                            <option value="AL">AL</option>
+                            <option value="AK">AK</option>
+                            <option value="AR">AR</option>
+                            <option value="AZ">AZ</option>
+                            <option value="CA">CA</option>
+                            <option value="CO">CO</option>
+                            <option value="CT">CT</option>
+                            <option value="DC">DC</option>
+                            <option value="DE">DE</option>
+                            <option value="FL">FL</option>
+                            <option value="GA">GA</option>
+                            <option value="HI">HI</option>
+                            <option value="IA">IA</option>
+                            <option value="ID">ID</option>
+                            <option value="IL">IL</option>
+                            <option value="IN">IN</option>
+                            <option value="KS">KS</option>
+                            <option value="KY">KY</option>
+                            <option value="LA">LA</option>
+                            <option value="MA">MA</option>
+                            <option value="MD">MD</option>
+                            <option value="ME">ME</option>
+                            <option value="MI">MI</option>
+                            <option value="MN">MN</option>
+                            <option value="MO">MO</option>
+                            <option value="MS">MS</option>
+                            <option value="MT">MT</option>
+                            <option value="NC">NC</option>
+                            <option value="NE">NE</option>
+                            <option value="NH">NH</option>
+                            <option value="NJ">NJ</option>
+                            <option value="NM">NM</option>
+                            <option value="NV">NV</option>
+                            <option value="NY">NY</option>
+                            <option value="ND">ND</option>
+                            <option value="OH">OH</option>
+                            <option value="OK">OK</option>
+                            <option value="OR">OR</option>
+                            <option value="PA">PA</option>
+                            <option value="RI">RI</option>
+                            <option value="SC">SC</option>
+                            <option value="SD">SD</option>
+                            <option value="TN">TN</option>
+                            <option value="TX">TX</option>
+                            <option value="UT">UT</option>
+                            <option value="VT">VT</option>
+                            <option value="VA">VA</option>
+                            <option value="WA">WA</option>
+                            <option value="WI">WI</option>
+                            <option value="WV">WV</option>
+                            <option value="WY">WY</option>
+                        </select>
+                        Comment:<br />
+                        <input type="text" id="picture-comment" />
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => {hideConfirmationModal(); openUploadModal();}}>
+                        Back
+                    </Button>
+                    <Button variant="primary" onClick={() => {hideConfirmationModal(); openSuccessModal()}}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showSuccessModal} onHide={hideSuccessModal} id="success-modal" animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Upload Successful!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" onClick={() => {hideSuccessModal(); openUploadModal();}}>
+                        Submit Another Claim
+                    </Button>
+                    <Button variant="primary" onClick={hideSuccessModal}>
+                        Exit to Map
                     </Button>
                 </Modal.Footer>
             </Modal>
