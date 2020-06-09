2ac7645569fe25b519f12ca0138ce3008aa72359
abachant <gbachant@gmail.com>
2020-06-09 09:13:10 -0400

    Add function to reformat datetime in <Report Table/> to be more readable

--- src/components/ReportTable.jsx
+++ src/components/ReportTable.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 function ReportTable(props) {
 
-    const markerList = props.markerList
+    const markerList = props.markerList;
+    
+    // Convert timestamp from exif data to a more readable formatted Date object
+    function formatDateTime(datetime){
+        // Trim non-numeric characters from timestamp
+        let date = datetime.split(' ')[0];
+        let time = datetime.split(' ')[1];
+        date = date.split(':');
+        time = time.split(':');
 
+        // Create Date object from input
+        let formattedDateTime = new Date(date[0], date[1], date[2], time[0], time[1], time[2])
+        const datetimeString = formattedDateTime.toLocaleString()
+        return datetimeString
+    }
     return (
         <div className="report-table-container col-md-4 hidden-xs">
             <table className="table">
@@ -12,14 +25,14 @@ function ReportTable(props) {
                         <th>Datetime</th>
                         <th>Locaton</th>
                         <th>License Plate</th>
-                        <th>license State</th>
+                        <th>License State</th>
                     </tr>
                 </thead>
                 <tbody>
                     {/* Create table entry for each incident in State */}
                     {markerList.map(item => (
-                        <tr key={item.key}>
-                            <td>{item.dateTime}</td>
+                        <tr key={item.name}>
+                            <td>{formatDateTime(item.dateTime)}</td>
                             <td>{[item.latitude, item.longitude]}</td>
                             <td>{item.licensePlate}</td>
                             <td>{item.state}</td>
