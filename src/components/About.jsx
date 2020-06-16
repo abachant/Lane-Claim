80cb8cadcd982b89a0e75e53e28cd3a1d21a9281
abachant <gbachant@gmail.com>
2020-06-16 07:59:37 -0400

    Update <About/> information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,14 @@ function About(props) {
                     <Modal.Title>About Lane Claim</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>Lane Claim is progressive web app dedicated to protecting bike lanes from automobile obstructions and holding offenders acountable.</p>
+                    <p>Lane Claim is progressive web app dedicated to protecting bike lanes from automobile obstructions.</p>
+                    <h3>Goals</h3>
+                    <ul>
+                        <li>To use this data to determine which bikelanes are in the most need for better protection</li>
+                        <li>To potentially hold offenders accountable</li>
+                    </ul>
+                    
+                    <h3>How To Use</h3>
                     <p>Simply take a picture of the offending vehicle and after submitting it Lane Claim will place a marker at its location with the offender's information.</p>    
                 </Modal.Body>
                 <Modal.Footer>
