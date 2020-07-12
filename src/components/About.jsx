c32c090a119dc71b30a446de3ac39e6cce7327fe
abachant <gbachant@gmail.com>
2020-07-12 11:01:18 -0400

    Update <About/> to include more info and bug reporting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,12 +22,28 @@ function About(props) {
                     <p>Lane Claim is progressive web app dedicated to protecting bike lanes from automobile obstructions.</p>
                     <h3>Goals</h3>
                     <ul>
-                        <li>To use this data to determine which bikelanes are in the most need for better protection</li>
-                        <li>To potentially hold offenders accountable</li>
+                        <li>To use this data to determine which bikelanes are in the most need for better protection.</li>
+                        <li>To potentially hold offenders accountable.</li>
                     </ul>
                     
                     <h3>How To Use</h3>
-                    <p>Simply take a picture of the offending vehicle and after submitting it Lane Claim will place a marker at its location with the offender's information.</p>    
+                    <ul>
+                        <li>Simply take a photo of the offending vehicle and upload it with the 'Submit New Claim' button</li>
+                        <li>Add any supplemental info like the offender's liscence plate or add a comment</li>
+                        <li>Lane Claim will place a marker at its location with its information</li>
+                    </ul>
+
+                    <h3>Open Source</h3>
+                    <ul>
+                        <li>Lane Claim is open source and pull requests are welcome!</li>
+                        <li>View the source code <a href="https://github.com/abachant/Lane-Claim">here</a>.</li>
+                    </ul>
+
+                    <h3>Submit a Bug</h3>
+                        <ul>
+                            <li><a href="mailto:laneclaim@gmail.com">Send an email</a></li>
+                            <li><a href="https://github.com/abachant/Lane-Claim/issues">Submit through GitHub</a></li>
+                        </ul>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={() => toggleAboutModal(false)}>
