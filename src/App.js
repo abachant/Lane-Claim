3d6647e925e0f0b907e1be5f21449efd96e27165
abachant <gbachant@gmail.com>
2020-07-25 21:16:47 -0400

    Add button to toggle display of <ReportTable />

--- src/App.js
+++ src/App.js
@@ -16,6 +16,16 @@ function App() {
   const [markerList, setMarkerList] = useState([ ]);
   const [toggleReportTable, setToggleReportTable] = useState(true);
 
+  let toggleSign;
+
+  switch (toggleReportTable) {
+    case true:
+      toggleSign = "－";
+      break;
+    case false:
+      toggleSign = "＋";
+      break;
+  }
   useEffect(() => {
     // Add each database entry from firebase to State
     database.on('value', (snapshot) => {
@@ -30,12 +40,12 @@ function App() {
       <div className="container-fluid row map">
           <div id="map-container">
             <Map className="map" center={[39.8283, -98.5795]} zoom={5}>
+            <button id="report-table-button" className="btn btn-secondary" onClick={() => setToggleReportTable(!toggleReportTable)} aria-controls="report-table-container" aria-expanded="toggleReportTable">{toggleSign}</button>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
             {/* <button className="btn btn-primary" data-toggle="collapse" data-target="#report-table-container">blumbpf</button> */}
-
             {/* Create Marker for each incident in State */}
             {markerList.map(item => (
               <Circle key={item.name} center={[item.latitude, item.longitude]} radius={8} className="marker">
@@ -85,7 +95,6 @@ function App() {
             )}
           </Map>
         </div>
-        <button className="btn btn-primary" onClick={() => setToggleReportTable(!toggleReportTable)} aria-controls="report-table-container" arai-expanded="toggleReportTable">Table</button>
         <Collapse in={toggleReportTable}>
           <div id="report-table-container" className="col-md-4 hidden-xs">
             <ReportTable markerList={markerList}/>  
